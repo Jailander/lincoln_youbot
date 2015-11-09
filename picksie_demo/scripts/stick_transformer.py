6df5a6a765714d979dc38f4cf23571a4f3dab320
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-11-09 23:08:16 +0000

    tf for stick

---
+++ picksie_demo/scripts/stick_transformer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from tf import TransformListener
+import geometry_msgs.msg
+
+
+class TransformS(object):
+    
+    def __init__(self):
+        rospy.init_node('stick_transform')
+        self.tf = TransformListener()
+
+        rospy.Subscriber("/whycon/poses",geometry_msgs.msg.PoseArray, self.handle_whycon)
+        self.stick_pose_pub = rospy.Publisher("/stick_pose", geometry_msgs.msg.PoseStamped, queue_size=1)
+    
+    def handle_whycon(self, msg):
+        for i in msg.poses:
+            stick_pose_or= geometry_msgs.msg.PoseStamped()
+            stick_pose_or.header=msg.header
+            stick_pose_or.pose=i
+            stick_pose_d= self.tf.transformPose('/base_link', stick_pose_or)
+            self.stick_pose_pub(stick_pose_d)
+
+
+if __name__ == '__main__':
+    teleop = TransformS()
+    rospy.spin()
